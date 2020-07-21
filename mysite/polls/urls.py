83b348c677094adba594cfaf4fa91b45b2e63a63
Abel Terefe <abelterefe98@gmail.com>
2020-07-20 20:11:50 -0500

    added more psudeocode

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,10 +1,12 @@
+# design URLs for an app
 # django.conf.urls contains utility functions
 from django.conf.urls import url
 #  ?in current (polls) directory import views
 from . import views
 
 urlpatterns = [
-  # ! url(regex, view, kwargs=None, name=None) 
-  # Strings typically use raw string syntax (r'') so that they can contain sequences like \d without the need to escape the backslash with another backslash.
+  # TODOurl(regex, view, kwargs=None, name=None) 
+  # The 'r' in front of each regular expression string tells Python that a string is “raw” – that nothing in the string should be escaped
+  # !There’s no need to add a leading slash, because every URL has that.
   url(r'^$', views.index, name='index')
 ]
