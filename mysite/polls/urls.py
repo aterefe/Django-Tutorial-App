bddb39678e1bc36b324c2e1d59fc965a206f93ca
Abel Terefe <abelterefe98@gmail.com>
2020-07-26 19:42:34 -0500

    added urls for user to see the result and question and vote

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,12 +1,20 @@
 # design URLs for an app
 # django.conf.urls contains utility functions
 from django.conf.urls import url
-#  ?in current (polls) directory import views
+#  in the current directory (polls) import views
 from . import views
 
+# Here we are adding the views to the polls url i.e polls/<view> 
 urlpatterns = [
   # TODOurl(regex, view, kwargs=None, name=None) 
   # The 'r' in front of each regular expression string tells Python that a string is “raw” – that nothing in the string should be escaped
   # !There’s no need to add a leading slash, because every URL has that.
-  url(r'^$', views.index, name='index')
+  # ex: /polls/
+  url(r'^$', views.index, name='index'),
+  # ex: /polls/<question_id>/
+  url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+  # ex: /polls/<question_id>/results
+  url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name="results"),
+  # ex:
+  url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote')
 ]
