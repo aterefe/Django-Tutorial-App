a6c72fdef4013eab4e415fede177e996e35a802b
Abel Terefe <abelterefe98@gmail.com>
2020-08-08 15:11:56 -0500

    convert detail and result view from python to django

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -14,3 +14,31 @@ class IndexView(generic.ListView):
   def get_queryset(self):
     """Return the last five published questions."""
     return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+  model = Question
+  template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+  model = Question
+  template_name = 'polls/detail.html'
+
+# The vote action handles voting for a particular choice in a particular question.
+def vote (request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    # request.POST is a dictionary-like object that lets you access submitted data by key name. request.POST values are always strings
+    # !request.POST['choice'] returns the ID of the selected choice
+    selected_choice = question.choice_set.get(pk=request.POST['choice'])
+  except (KeyError, Choice.DoesNotExist):
+    # Redisplay the question Voting form.
+    return render(request, 'polls/detail.html', {
+      'question': question,
+      'error_message': "You didn't select a choice."
+    })
+  else:
+    selected_choice.votes += 1
+    selected_choice.save()
+    # Always return an HttpResponseRedirect after successfully dealing with POST data. 
+    # This prevents data from being posted twice if a user hits the Back button.
+    return HttpResponseRedirect(reverse('results', args=(question.id,)))
