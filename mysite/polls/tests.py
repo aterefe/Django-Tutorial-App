55abc56c26b96e48e573ee24af64697c8603ad6d
Abel Terefe <abelterefe98@gmail.com>
2020-08-08 16:29:53 -0500

    created a django.test.TestCase subclass

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,16 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Question
+
+# TODO: Created a django.test.TestCase subclass with a method that creates a Question instance with a pub_date in the future
+class QuestionModelTest(TestCase):
+  def test_was_published_recently_with_future_question(self):
+    """
+    was_published_recently() returns False for question whose pub_date is in the future
+    """
+    time = timezone.now() + datetime.timedelta(days=30)
+    future_question = Question(pub_date=time)
+    self.assertIs(future_question.was_published_recently(), False)
